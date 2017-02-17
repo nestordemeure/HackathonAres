c0a5cfee4ad41fb61d1e41a70ad15ce65afab28a
nestordemeure <nestordemeure@gmail.com>
2017-02-18 00:24:38 +0100

    explo

--- ares/Ares/explo.fs
+++ ares/Ares/explo.fs
@@ -4,3 +4,18 @@ open Influence
 
 let rng = new System.Random()
 
+//-------------------------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------------------------
+
+let explo x y (field:InfluenceField) (client:InfluenceClient) distanceDeSec =
+   let cell = field.GetCell(x, y)
+   let dx = cell.GetX() + rng.Next(3) - 1
+   let dy = cell.GetY() + rng.Next(3) - 1
+   if dx >= 0 && dx < field.GetWidth() && dy >= 0 && dy < field.GetHeight() then 
+      let cellToAttack = field.GetCell(dx, dy)
+      if (isNull cellToAttack |> not) && (cellToAttack.GetOwner() <> client.GetNumber()) then 
+         Some (dx,dy)
+      else None
+   else None
