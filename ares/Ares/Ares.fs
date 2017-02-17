bed8224c216e13647fed2f65d324a99f70805a17
nestordemeure <nestordemeure@gmail.com>
2017-02-17 23:17:28 +0100

    comp

--- ares/Ares/Ares.fs
+++ ares/Ares/Ares.fs
@@ -5,32 +5,33 @@ open Influence
 
 [<EntryPoint>]
 let main argv =
-   let r = new System.Random()
+   let rng = new System.Random()
    let client = InfluenceClient.GetInstance()
-   //client.Connect("127.0.0.1", "les DauF#ins Surfeurs")
-   client.Connect("10.3.4.37", "les DauF#ins Surfeurs");
+   client.Connect("127.0.0.1", "les DauF#ins Surfeurs")
+   //client.Connect("10.3.4.37", "les DauF#ins Surfeurs")
    let mutable field = client.NextRound()
    let mutable myCells = client.GetMyCells()
 
    while client.GetStatus() = InfluenceClient.Status.ONGOING do
+
       printfn "%A - Attacking" DateTime.Now
-      for i = 0 to 10 do
+      for attackNumber = 1 to 20 do
          myCells <- client.GetMyCells()
-         let c = myCells.[r.Next(myCells.Count)]
-         if c.GetUnitsCount() >= 2 then 
-            let dx = c.GetX() + r.Next(3) - 1
-            let dy = c.GetY() + r.Next(3) - 1
+         let cell = myCells.[rng.Next(myCells.Count)]
+         if cell.GetUnitsCount() >= 2 then 
+            let dx = cell.GetX() + rng.Next(3) - 1
+            let dy = cell.GetY() + rng.Next(3) - 1
             if dx >= 0 && dx < field.GetWidth() && dy >= 0 && dy < field.GetHeight() then 
                let cellToAttack = field.GetCell(dx, dy)
-               if (isNull cellToAttack) && (cellToAttack.GetOwner() <> client.GetNumber()) then 
-                  field <- client.Attack(c.GetX(), c.GetY(), cellToAttack.GetX(), cellToAttack.GetY())
+               if (isNull cellToAttack |> not) && (cellToAttack.GetOwner() <> client.GetNumber()) then 
+                  field <- client.Attack(cell.GetX(), cell.GetY(), cellToAttack.GetX(), cellToAttack.GetY())
 
       let unitsToAdd = client.EndAttacks()
       myCells <- client.GetMyCells()
-      for i = 0 to unitsToAdd do
-         let c = myCells.[r.Next(myCells.Count)]
-         client.AddUnits(c, 1)
-         field <- client.EndAddingUnits()
+      for renfortNum = 1 to unitsToAdd do
+         let cell = myCells.[rng.Next(myCells.Count)]
+         client.AddUnits(cell, 1)
+      field <- client.EndAddingUnits()
 
       match client.GetStatus() with 
       | InfluenceClient.Status.VICTORY -> printfn "YOU WON!"
