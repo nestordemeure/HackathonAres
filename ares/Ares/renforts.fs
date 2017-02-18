8cd08ffdc236d2fba3d778b3df083197cc0a8bb5
nestordemeure <nestordemeure@gmail.com>
2017-02-18 05:12:26 +0100

    r

--- ares/Ares/renforts.fs
+++ ares/Ares/renforts.fs
@@ -79,22 +79,31 @@ let renforce (field:InfluenceField) (client:InfluenceClient) unitsToAdd =
       let mutable stop = false
       let mutable renforceThis = true
       // scouts (1 sur 2)
-      while renfortNum < unitsToAdd && not stop do 
-         match scouts with 
-         | [] -> stop <- true
+      match fighters with 
+         | [] ->
+            while renfortNum < unitsToAdd && not stop do 
+                  match scouts with 
+                  | [] -> stop <- true
+                  | (x,y)::q ->
+                        client.AddUnits(field.GetCell(x,y), 1)
+                        renfortNum <- renfortNum + 1
+                        scouts <- insertLast q (x,y)
          | (x,y)::q ->
-            if renforceThis then
-                  client.AddUnits(field.GetCell(x,y), 1)
-            renfortNum <- renfortNum + 1
-            renforceThis <- not renforceThis
-            scouts <- q
+            while renfortNum < unitsToAdd && not stop do 
+            match scouts with 
+            | [] -> stop <- true
+            | (x,y)::q ->
+                  if renforceThis then
+                        client.AddUnits(field.GetCell(x,y), 1)
+                  renfortNum <- renfortNum + 1
+                  renforceThis <- not renforceThis
+                  scouts <- q
       // fighters
       stop <- false
-      while renfortNum < unitsToAdd && not stop do 
+      while renfortNum < unitsToAdd && not stop do
          match fighters with 
          | [] -> stop <- true
          | (x,y)::q -> 
             client.AddUnits(field.GetCell(x,y), 1)
             renfortNum <- renfortNum + 1
             fighters <- insertLast q (x,y)
-
